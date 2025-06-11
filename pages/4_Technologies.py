938b88ca23dff712b936c634db52cc200ded7e82
peterrgithub <143199747+peterrgithub@users.noreply.github.com>
2025-06-11 22:21:48 +0100

    4_Technologies.py
    Fix: Display pictures with proportion to their matching texts.
    
    This commit addresses a bug where users were unable to view a proportionate picture for its corresponding texts on the page.
    
    The bug was caused by a disproportionate arrangement without proper organization in the code base. This commit adds a container per item, replaces image and text columns with left and right columns for an even more conspicuous picture display.

--- pages/4_Technologies.py
+++ pages/4_Technologies.py
@@ -56,15 +56,15 @@ local_css("style/style.css")
 #=====Loading Assets======
 
 
-    #My Services
+    #Our Services
 with st.container():
     st.write("---")
-    st.title("Technologies")
+    st.title("Study Abroad")
     left_column, right_column = st.columns(2)
     with left_column:
         st.write(
             """
-            FIBRE OPTIC POWERED NETWORKS:
+            **FIBRE OPTIC POWERED NETWORKS:**
             With the extreme internet network technology quality that fibre optic cables bring to the table, we are leveraging this and providing organizations,
             schools, busnesses and even religious institutions with high-speed-of-light internet network installation services. These are in other words called
             conventionally as Wirled Local Area Networks. You could go ahead, give us a call and try that out for yourself. Highlight features include;
@@ -74,97 +74,100 @@ with st.container():
             - Effective deliery of maintenance routines e.t.c.
             """
         )
-        st.write("[Check our Facebook page for more related posts](https://www.facebook.com/GoldlionServices/)")
-        
+        st.write("[Check our Facebook page for more related posts](https://www.facebook.com/GoldlionServices/)") 
          
+
     with right_column:
         st.image("fibreoptic2.jpg")
-        st.image("fibreoptic1.jpg")
-        st.write("##")
+        st.image("fibreoptic2.jpg")
+        
+                  
+    with st.container():
+        st.write("---")
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.image("websitewithpython1.jpeg")
+        
+        with right_column:
+            st.write(
+                """
+                **PYTHON-BASED WEBSITE DESIGN:**
+                With a strong desire to be unique and make a difference, one of our goals and ambition is to revolutionize and promote the use of python in
+                Software Engineering as a whole, starting with website design, we are striving hard to continue to improve the usage of python in a novel way (with
+                streamlit instead of flask) to desgin websites that are not only responsive too but just as good as those designed with HTML, functional as
+                JavaScript-used and PhP-used websites, stylish as CSS-used websites and database driven as SQL-used websites - making sure that the python
+                revolution is alive. Highlight features include;
+                - Streamlit
+                - Pillow
+                - CSS
+                - MySQLite
+                """
+            )
+        
+            st.write("[Check our Facebook page for more related posts](https://www.facebook.com/GoldlionServices/)")
 
+
+
+with st.container():
+    st.write("---")
     
-    with right_column:
-        st.write(
-            """
-            PYTHON-BASED WEBSITE DEISGN:
-            With a strong desire to be unique and make a difference, one of our goals and ambition is to revolutionize and promote the use of python in Software
-            Engineering as a whole, starting with website design, we are striving hard to continue to improve the usage of python in a novel way (with streamlit
-            instead of flask) to desgin websites that are not only responsive too but just as good as those designed with HTML, functional as JavaScript-used and
-            PhP-used websites, stylish as CSS-used websites and database driven as SQL-used websites - making sure that the python revolution is alive. Highlight
-            features include;
-            - Streamlit
-            - Pillow
-            - CSS
-            - MySQLite
-            """
-        )
-        st.write("[Check our Facebook page for more related posts](https://www.facebook.com/GoldlionServices/)") 
-         
+    left_column, right_column = st.columns(2)
     with left_column:
-        st.write("##")
-        st.write("##")
-        st.image("websitewithpython1.jpeg")
-
-
-
-    with left_column:
-        st.write("##")
-        st.write("##")
-        st.write("##")
         st.write(
             """
-            PYTHON-BASED MOBILE APP DEVELOPMENT:
-            With a strong desire to be unique and make a difference, one of our goals and ambition is to revolutionize and promote the use of python in Software
-            Engineering as a whole, featuring mobile App development, we are striving hard to continue to improve the usage of python in a novel way to develop
-            mobile Apps that are not only responsive too but just as good as those designed with Kotlin, functional as Android-used and Swift-used mobile Apps,
-            stylish as flutter-used mobile Apps and database driven as SQL-used mobile Apps - making sure that the python revolution is alive. Highlight
-            features include;
-            - Kivy
-            - Pillow
-            - Tkinter
-            - MySQLite
-            """
-        )
-        st.write("[Check our Facebook page for more related posts](https://www.facebook.com/GoldlionServices/)") 
-         
-    with right_column:
-        st.write("##")
-        st.write("##")
-        st.image("kivy1.png")
-
-
-    with right_column:
-        st.write("##")
-        st.write("##")
-        st.write("##")
-        st.write("##")
-        st.write(
-            """
-            VIDEO DOORBELLS:
+            **VIDEO DOORBELLS:**
             With security threats, sneak attacks skyrocktting in today's world, a step up in security measures is now everyone's imperative. That is why we come in
-            installation of security video door bells to guarantee visual representation of all breaches, leaving precise evidences for both plaintiffs and law
-            enforcements agancies to act on. Highlight features include;
+            with installation of security video door bells to guarantee visual representation of all breaches, leaving precise evidences for both plaintiffs and
+            law enforcements agancies to act on. Highlight features include;
             - Long distance coverage as far as a 250 meters
             - Strong camera capturing lenses
             - Water resistance to withstand harsh rainy weathers
             - Wide or divergent scope of coverage e.t.c.
             """
         )
-        st.write("[Check our Facebook page for more related posts](https://www.facebook.com/GoldlionServices/)")
         
+        st.write("[Check our Facebook page for more related posts](https://www.facebook.com/GoldlionServices/)") 
+ 
          
-    with left_column:
-        st.write("##")
-        st.write("##")
-        st.image("vidoorbell.png")
-        
-        
 
+    with right_column:
+        st.image("vidoorbell.png")
+
+    
+    with st.container():
+        st.write("---")
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.image("kivy1.png")
+            
+        
+        with right_column:
+            st.write(
+                """
+                **PYTHON-BASED MOBILE APP DEVELOPMENT AND OTHER LANGUAGES:**
+                With a strong desire to be unique and make a difference, one of our goals and ambition is to revolutionize and promote the use of python in
+                Software Engineering as a whole, featuring mobile App development, we are striving hard to continue to improve the usage of python in a novel way
+                to develop mobile Apps that are not only responsive too but just as good as those designed with Kotlin, functional as Android-used and Swift-used
+                mobile Apps, stylish as flutter-used mobile Apps and database driven as SQL-used mobile Apps - making sure that the python revolution is alive.
+                Notwithstanding, we still do mobile App projects with other languages too. Highlight features include;
+                - Kivy
+                - Pillow
+                - Tkinter
+                - MySQLite
+                - Kotlin
+                - Swift
+                - Flutter e.t.c.
+                """
+            )
+        
+            st.write("[Check our Facebook page for more related posts](https://www.facebook.com/GoldlionServices/)")
+
+        
     with st.container():
         st.write("---")
         text_column1, text_column2, text_column3 = st.columns((1, 2, 3))
         with text_column1:
-            st.header("Pay")
+            st.subheader("Pay")
             st.write(
                 """
                 Cards\n
@@ -176,7 +179,7 @@ with st.container():
                 """
             )
             
-            st.header("License")
+            st.subheader("License")
             st.write(
                 """
                 RC: PL20420
@@ -184,7 +187,7 @@ with st.container():
             )
             
         with text_column2:
-            st.header("Refund Policy")
+            st.subheader("Refund Policy")
             st.write(
                 """
                 Any client seeking for a refund must make such request prior to my submission of the first phase of work; if the first phase commences before a
@@ -192,7 +195,7 @@ with st.container():
                 """
             )
             
-            st.header("Location")
+            st.subheader("Location")
             st.write(
                 """
                 16, Gut Central, Along Rayfield Resort Road, Off P.R.T.V. Roundabout, Rayfield, Jos, Plateau State, Nigeria
@@ -200,7 +203,7 @@ with st.container():
             )
             
         with text_column3:
-            st.header("Partners")
+            st.subheader("Partners")
             st.write(
                 """
                 Sheltonville Schools, Jos  |  Emmanuel International College, Jos  |  Day-by-Day Academy, Abuja  |  Jorola Integrations
